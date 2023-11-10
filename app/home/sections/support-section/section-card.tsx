0bd214ac30c2cb62ba52d99e4da600b23ff4dcf6
FreeseBee <ajakub2000@interia.pl>
2023-11-11 00:16:44 +0100

    Support section in Home page

---
+++ app/home/sections/support-section/section-card.tsx
@@ -0,0 +1,57 @@
+import { Button, Card, Nav } from "react-bootstrap";
+import Link from "next/link";
+import { useRouter } from 'next/navigation';
+
+export interface SectionCardData {
+  title: string;
+  description: string;
+  lightImgSrc: string;
+  darkImgSrc: string;
+  display: boolean;
+  link: string;
+  buttonLabel: string;
+}
+
+type Props = { data: SectionCardData };
+
+export default function SectionCard({ data }: Props) {
+  const router = useRouter();
+
+  return (
+    <Card className="d-flex justify-content-center" style={{ height: "100%" }}>
+      <Card.Img
+        fetchPriority="high"
+        variant="top"
+        src={data.lightImgSrc}
+        style={{ objectFit: "cover", height: 150 }}
+        className="d-none d-md-block"
+      />
+      <Card.Img
+        fetchPriority="high"
+        variant="top"
+        src={data.darkImgSrc}
+        style={{ objectFit: "cover", height: 170 }}
+        className="d-block d-md-none rounded-bottom"
+      />
+      <Nav.Link as={Link} href={data.link} disabled={false}>
+        <Card.ImgOverlay className={"d-block d-md-none text-light"}>
+          <Card.Title className="img-overlay">
+            <h1>{data.title}</h1>
+          </Card.Title>
+          <Card.Text className="img-overlay">{data.description}</Card.Text>
+        </Card.ImgOverlay>
+      </Nav.Link>
+      <Card.Body className="d-none d-md-block">
+        <Card.Title>{data.title}</Card.Title>
+        <Card.Text>{data.description}</Card.Text>
+        <Button
+          variant="primary"
+          className="button"
+          onClick={() => router.push(data.link)}
+        >
+          {data.buttonLabel}
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,11 @@
 "use client";
-import Link from "next/link";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { useRouter } from 'next/navigation';
 import caveBg from "public/images/Sample_image_dark.png";
-import { navigations, Links } from "./components/navbar/navigation";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { Links, navigations } from "./components/navbar/navigation";
+
 export default function NotFound() {
+  const router = useRouter();
   const homeNavigation = navigations.find((n) => n.link == Links.HOME);
   return (
     <Container fluid>
@@ -27,7 +29,10 @@ export default function NotFound() {
           <div className="text-center text-md-start">
             <h1 className="info">Ups... Chyba zabłądziłeś</h1>
             <h6>Kliknij kafelek by wrócić na powierzchnię!</h6>
-            <Button className="button" href={homeNavigation.link}>
+            <Button
+              className="button-radioactive"
+              onClick={() => router.push(homeNavigation.link)}
+            >
               {homeNavigation.name}
             </Button>
           </div>
