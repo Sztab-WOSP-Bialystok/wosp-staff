5a7960b499d20edc9abd4f6b3bb03eeb1fdbdba8
FreeseBee <ajakub2000@interia.pl>
2023-11-13 22:21:29 +0100

    Added custom volounteer FAQ header

--- app/faq/components/Questions.tsx
+++ app/faq/components/Questions.tsx
@@ -10,7 +10,7 @@ const Questions: FC<QuestionsProps> = ({ questions }: QuestionsProps) => {
 
     return <Stack gap={2}>
         {questions.map(({ question, answer }, index) => (
-            <div key={question + index} className="m-3">
+            <div key={question + index} className="my-1 mx-0 mx-sm-3">
                 <p><strong>{question}</strong></p>
                 <p>{answer}</p>
             </div>
