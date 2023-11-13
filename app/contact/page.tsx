3118af581d52dfbfca0a4edbc2f9747983b2b441
FreeseBee <ajakub2000@interia.pl>
2023-11-13 23:10:06 +0100

    Added contact page header

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -10,6 +10,18 @@ export default function Page() {
   return (
     <Container>
       <Row>
+        <Col>
+          <h1 className="my-3">Godziny otwarcia siedziby Sztabu</h1>
+          <h3 className="my-3">Środa i piątek 17:00-20:00</h3>
+          <h4 className="my-3">ul. Warszawska 50 lok. 206, Białystok</h4>
+          <h5 className="my-3 theme-pink">
+            UWAGA! W celu umówienia&nbsp;się&nbsp;na spotkanie bądź dostarczenia przedmiotu&nbsp;na
+            aukcje&nbsp;poza podanymi godzinami prosimy&nbsp;o wcześniejszy kontakt
+            telefoniczny&nbsp;z odpowiednim koordynatorem
+          </h5>
+        </Col>
+      </Row>
+      <Row>
         {data.map((data) => (
           <Col xs={12} sm={12} md={6} lg={4} className="mb-4">
             <ContactCard data={data}></ContactCard>
