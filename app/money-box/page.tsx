3994e3c6bc0390e82bc435cfd85edb2668f57e13
FreeseBee <ajakub2000@interia.pl>
2023-12-22 01:05:11 +0100

    Added Accordions to Stationary Money-box page

--- app/money-box/page.tsx
+++ app/money-box/page.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Accordion } from "react-bootstrap";
 import contactData from "../../public/info/contact-data.json";
 import ContactCard, { ContactData } from "../contact/contact-card";
 import Link from "next/link";
-
 //TODO: Configure SSR
 
 export default function Page() {
@@ -17,59 +16,86 @@ export default function Page() {
   return (
     <Container className="my-3 my-xl-5">
       <Row>
-        <Col xs={12}>
-          <h3>
+        <Col xs={12} className="text-center">
+          <h3 >
             Czy wiesz, że&nbsp;każda firma i&nbsp;instytucja może wspomóc
             zbiórkę WOŚP?
           </h3>
-          <p>
+          <p className="px-5">
             Można to&nbsp;zrobić poprzez Puszkę Stacjonarną, którą można mieć we
-            własnym lokalu od 10 grudnia 2023 do&nbsp;dnia Finału w&nbsp;dniu 28
-            stycznia 2024.
+            własnym lokalu od 10 grudnia 2023 do&nbsp;dnia Finału w&nbsp;dniu 28&nbsp;stycznia&nbsp;2024.
           </p>
         </Col>
         <Col xs={12} className="my-3 my-xl-5">
-          <h5>Kto może ubiegać się o Puszkę Stacjonarną?</h5>
-          <p>
-            Każda firma i&nbsp;instytucja. Osoby prywatne nie mogą uzyskać
-            Puszki Stacjonarnej.
-          </p>
-          <h5>Czym jest Firmowa Puszka Stacjonarna?</h5>
-          <p>
-            Firmowa Puszka Stacjonarna to tekturowa puszka kwestarska, która
-            przypisana jest do konkretnego Opiekuna i&nbsp;lokalu. Sztab WOŚP
-            prowadzi nadzór nad prowadzoną kwestą (wydaje niezbędne materiały
-            oraz rozlicza zbiórkę). Zbiórka do Firmowych Puszek Stacjonarnych
-            może odbywać się w dniach 10.12.2023 r. – 28.01.2024.
-          </p>
-          <h5>Jak mogę zarejestrować Puszkę Stacjonarną?</h5>
-          <h6>
-            <span>Przez portal: </span>
-            <span>
-              <Link href="https://iwolontariusz.wosp.org.pl/">
-                iwolontariusz.wosp.org.pl
-              </Link>
-            </span>
-          </h6>
-          <p>
-            Aby to zrobić należy: założyć i potwierdzić konto (jeśli nie było
-            zakładane), następnie przejść na stronę rejestracji, wybrać opcję
-            "CHCĘ REPREZENTOWAĆ FIRMĘ", wypełnić dane firmy, znaleźć sztab
-            Białystok #6413, podać termin zbiórki oraz lokalizację swojej firmy,
-            gdzie będzie znajdować się Puszka. Na tym etapie można dodać kolejną
-            puszkę, oraz dodatkowo założyć eSkarbonkę. Na końcu następuje
-            weryfikacja i akceptacja wniosku.
-          </p>
-          <h6>Przez papierowy formularz rejestracyjny.</h6>
-          <p>
-            <span>Aby go otrzymać należy napisać do nas na: </span>
-            <a href={"mailto:" + mainStaffContact.mail}>
-              {mainStaffContact.mail}{" "}
-            </a>
-            <span>
-              lub skontaktować się z koordynatorem ds Puszek Stacjonarnych:
-            </span>
-          </p>
+          <Accordion defaultActiveKey={['0']}>
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>
+                Czym jest Firmowa Puszka Stacjonarna?
+              </Accordion.Header>
+              <Accordion.Body>
+                Firmowa Puszka Stacjonarna to tekturowa puszka kwestarska, która
+                przypisana jest do konkretnego Opiekuna i&nbsp;lokalu. Sztab
+                WOŚP prowadzi nadzór nad prowadzoną kwestą (wydaje niezbędne
+                materiały oraz rozlicza zbiórkę). Zbiórka do Firmowych Puszek
+                Stacjonarnych może odbywać się w dniach 10.12.2023 r. –
+                28.01.2024.
+              </Accordion.Body>
+            </Accordion.Item>
+
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>
+                Kto może ubiegać się o Puszkę Stacjonarną?
+              </Accordion.Header>
+              <Accordion.Body>
+                Każda firma i&nbsp;instytucja. Osoby prywatne nie mogą uzyskać
+                Puszki Stacjonarnej.
+              </Accordion.Body>
+            </Accordion.Item>
+
+            <Accordion.Item eventKey="2">
+              <Accordion.Header>
+                Jak mogę zarejestrować Puszkę Stacjonarną?
+              </Accordion.Header>
+              <Accordion.Body>
+                <div>
+                  <h6>
+                    <span>Przez portal: </span>
+                    <span>
+                      <Link href="https://iwolontariusz.wosp.org.pl/">
+                        iwolontariusz.wosp.org.pl
+                      </Link>
+                    </span>
+                  </h6>
+                  <span>
+                    Aby to zrobić należy: założyć i potwierdzić konto (jeśli nie
+                    było zakładane), następnie przejść na stronę rejestracji,
+                    wybrać opcję "CHCĘ REPREZENTOWAĆ FIRMĘ", wypełnić dane
+                    firmy, znaleźć sztab Białystok #6413, podać termin zbiórki
+                    oraz lokalizację swojej firmy, gdzie będzie znajdować się
+                    Puszka. Na tym etapie można dodać kolejną puszkę, oraz
+                    dodatkowo założyć eSkarbonkę. Na końcu następuje weryfikacja
+                    i akceptacja wniosku.
+                  </span>
+                </div>
+                <div className="mt-4">
+                  <h6>Przez papierowy formularz rejestracyjny.</h6>
+                  <span>Aby go otrzymać należy napisać do nas na: </span>
+                  <a href={"mailto:" + mainStaffContact.mail}>
+                    {mainStaffContact.mail}
+                  </a>
+                  <span>
+                    {" "}
+                    lub skontaktować się z koordynatorem ds Puszek
+                    Stacjonarnych:
+                  </span>
+                </div>
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12}>
           <Container>
             <Row>
               <Col
