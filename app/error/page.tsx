1e2f1ddadddae510f6e4be18d76949518794f0e3
Michał Bołtowicz <micbo26@gmail.com>
2023-12-31 18:11:45 +0100

    router refresh instead of back

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -6,24 +6,20 @@ type Props = { error: string; errorInfo: any };
 
 export default function ErrorPage({ error, errorInfo }: Props) {
   const router = useRouter();
-  const handleBack = () => {
-    console.log(errorInfo)
-    //router.back();
+  const handleClick = () => {
+    console.log(errorInfo);
+    router.refresh();
   };
 
   return (
-          <div>
-            <h2>Oops, wygląda na to, że doszło do błędu! {error && error.toString()}</h2>
-            <p>{error && error.toString()}</p>
-            <button
-              type="button"
-              onClick={
-                handleBack
-              }
-            >
-              Wróć
-            </button>
-          </div>
-
+    <div>
+      <h2>
+        Oops, wygląda na to, że doszło do błędu! {error && error.toString()}
+      </h2>
+      <p>{error && error.toString()}</p>
+      <button type="button" onClick={handleClick}>
+        Wróć
+      </button>
+    </div>
   );
 }
