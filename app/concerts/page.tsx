b370aa3bb3ca776d6c0b04cfa4fbc8c559a9f879
TheUnDc <138985183+TheUnDc@users.noreply.github.com>
2024-01-09 20:46:55 +0100

    deleted unessesary description adn adjusted header

--- app/concerts/page.tsx
+++ app/concerts/page.tsx
@@ -4,23 +4,19 @@ import concertsPoster from "../../public/images/ConcertsPoster.jpg";
 
 export default function Page() {
 	return (
-		<Container className="mt-2 mt-sm-3 mt-xl-5">
-			<Row>
-				<Col
-					lg={{ span: 8, offset: 2 }}
-					className="text-center img-overlay"
-				>
-					<h1>Koncerty</h1>
-					<p className="px-5">
-						Lorem, ipsum dolor sit amet consectetur adipisicing
-						elit. Omnis veritatis est quidem quo dignissimos nisi
-						enim commodi ducimus cupiditate totam illo voluptatibus
-						voluptatum quas id soluta impedit, ratione inventore
-						esse.
-					</p>
-					<img src={concertsPoster.src} width={"100%"} />
-				</Col>
-			</Row>
+		<Container className="my-3 my-xl-5">
+			<Col
+				md={{ span: 8, offset: 2 }}
+				className="text-center img-overlay"
+			>
+				<h1 className="mb-4">Koncerty</h1>
+			</Col>
+			<Col
+				lg={{ span: 8, offset: 2 }}
+				className="text-center img-overlay"
+			>
+				<img src={concertsPoster.src} width={"100%"} />
+			</Col>
 		</Container>
 	);
 }
