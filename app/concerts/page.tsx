b1a2e8a4cfefb0fcd8c187613b1841f68d953e51
TheUnDc <138985183+TheUnDc@users.noreply.github.com>
2024-01-09 20:34:05 +0100

    add img

--- app/concerts/page.tsx
+++ app/concerts/page.tsx
@@ -4,27 +4,23 @@ import concertsPoster from "../../public/images/ConcertsPoster.jpg";
 
 export default function Page() {
 	return (
-			<Container className="mt-2 mt-sm-3 mt-xl-5">
-				<Row>
-					<Col
-						lg={{ span: 8, offset: 2 }}
-						className="text-center img-overlay"
-					>
-						<h1>Koncerty</h1>
-						<p className="px-5">
-							Lorem, ipsum dolor sit amet consectetur adipisicing
-							elit. Omnis veritatis est quidem quo dignissimos
-							nisi enim commodi ducimus cupiditate totam illo
-							voluptatibus voluptatum quas id soluta impedit,
-							ratione inventore esse.
-						</p>
-					</Col>
-				</Row>
-				<Row>
-					<Col xl={{ span: 8, offset: 2 }} className="my-3">
-					
-					</Col>
-				</Row>
-			</Container>
+		<Container className="mt-2 mt-sm-3 mt-xl-5">
+			<Row>
+				<Col
+					lg={{ span: 8, offset: 2 }}
+					className="text-center img-overlay"
+				>
+					<h1>Koncerty</h1>
+					<p className="px-5">
+						Lorem, ipsum dolor sit amet consectetur adipisicing
+						elit. Omnis veritatis est quidem quo dignissimos nisi
+						enim commodi ducimus cupiditate totam illo voluptatibus
+						voluptatum quas id soluta impedit, ratione inventore
+						esse.
+					</p>
+					<img src={concertsPoster.src} width={"100%"} />
+				</Col>
+			</Row>
+		</Container>
 	);
 }
