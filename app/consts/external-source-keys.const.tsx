a7fe9b6dfda7e846eec19067a10ae7c8ff0c48c5
FreeseBee <ajakub2000@interia.pl>
2024-01-10 01:23:24 +0100

    Added marathons page Facebook event redirect

--- app/consts/external-source-keys.const.tsx
+++ app/consts/external-source-keys.const.tsx
@@ -2,4 +2,5 @@ export const EXTERNAL_SOURCE_KEYS = {
     VOLOUNTEER: 'for_volounteer',
     BIALYSTOK_WEB: 'bialystok_web',
     MARATHON_FORM: 'marathon_form',
+    MARATHON_FB_EVENT: 'marathon_fb_event',
 }
